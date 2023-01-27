04892c1621b9b1fa6a9c53dc645a44a4f7847ac7
WilPaD <wilbertjose10@gmail.com>
2023-01-27 13:55:45 -0600

    Desestructuracion de objetos

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-const person = {
-    name: "Tony",
-    age: 45,
-    codeName: 'Iron Man'
-}
 
-const { age, name, codeName, power  = "No tiene poder" } = person
-
-console.log(name)
-console.log(age)
-console.log( codeName)
-console.log(power)
+console.log('Hola Mundo')
