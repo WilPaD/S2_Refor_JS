4705da4c41cb985a6bad4f7e146e16f3d552a4d1
WilPaD <wilbertjose10@gmail.com>
2023-01-27 13:36:08 -0600

    Impresion individual  de los datos de la estructura

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+const person = {
+    name: "Tony",
+    age: 45,
+    codeName: 'Iron Man'
+}
 
-console.log('hola')
+const { age, name, codeName, power  = "No tiene poder" } = person
+
+console.log(name)
+console.log(age)
+console.log( codeName)
+console.log(power)
