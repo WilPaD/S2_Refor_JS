1498e30b22104ba62b3c9aa17e770c6112b4fce0
WilPaD <wilbertjose10@gmail.com>
2023-01-28 19:12:03 -0600

    Verificando el cambio de url por cambio de nombre al repositorio

---
+++ src/bases/14-ternario.js
@@ -0,0 +1,19 @@
+
+let firstName 
+let lastName = "Padrón"
+
+/** Verifica si la variable tiene un valor, imprime dependiendo el contenido de la variable  */
+//console.log(`${ firstName || 'No first name' } ${ lastName || 'No last name'}`)
+
+const isActive = true 
+
+let message = (isActive ) ? 'Activo' : 'Inactivo' //Operador ternario (If )
+/*
+if( isActive ){
+    message = 'Activo'
+}else{
+    message 'Inactivo'
+}
+*/
+
+console.log(message)
